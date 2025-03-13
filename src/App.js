125f0b615406a1a2e7953d3ae001e8b9eaa3fbf4
Ahmed Maher <AMaherJK@GMail.com>
2025-03-14 00:29:21 +0200

    UI basics established

--- src/App.js
+++ src/App.js
@@ -3,7 +3,71 @@ import Item from './Item';
 function App() {
   return (
     <div className="App">
-      <Item name="فلان الفلاني"/>
+      <div className='GameContainer'>
+        <div className='BracketContainer'>
+          <div className='RO16ContainerB'>
+            <div className='RO16ContainerS'>
+              <Item name="فلان الفلاني" />
+              <Item name="فلان الفلاني" />
+            </div>
+            <div className='RO16ContainerS'>
+              <Item name="فلان الفلاني" />
+              <Item name="فلان الفلاني" />
+            </div>
+            <div className='RO16ContainerS'>
+              <Item name="فلان الفلاني" />
+              <Item name="فلان الفلاني" />
+            </div>
+            <div className='RO16ContainerS'>
+              <Item name="فلان الفلاني" />
+              <Item name="فلان الفلاني" />
+            </div>
+
+          </div>
+          <div className='RO8ContainerB'>
+            <Item name="فلان الفلاني" />
+            <Item name="فلان الفلاني" />
+            <Item name="فلان الفلاني" />
+            <Item name="فلان الفلاني" />
+          </div>
+          <div className='RO4ContainerB'>
+            <Item name="فلان الفلاني" />
+            <Item name="فلان الفلاني" />
+          </div>
+          <Item name="فلان الفلاني" />
+          <Item name="فلان الفلاني" />
+          <div className='RO4ContainerB'>
+            <Item name="فلان الفلاني" />
+            <Item name="فلان الفلاني" />
+          </div>
+          <div className='RO8ContainerB'>
+            <Item name="فلان الفلاني" />
+            <Item name="فلان الفلاني" />
+            <Item name="فلان الفلاني" />
+            <Item name="فلان الفلاني" />
+          </div>
+          <div className='RO16ContainerB'>
+            <div className='RO16ContainerS'>
+              <Item name="فلان الفلاني" />
+              <Item name="فلان الفلاني" />
+            </div>
+            <div className='RO16ContainerS'>
+              <Item name="فلان الفلاني" />
+              <Item name="فلان الفلاني" />
+            </div>
+            <div className='RO16ContainerS'>
+              <Item name="فلان الفلاني" />
+              <Item name="فلان الفلاني" />
+            </div>
+            <div className='RO16ContainerS'>
+              <Item name="فلان الفلاني" />
+              <Item name="فلان الفلاني" />
+            </div>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
