b18284b6a1f4d27023d795f605de1f80e6697824
Ahmed Maher <AMaherJK@GMail.com>
2025-03-16 21:51:29 +0200

    added some art and edited some metadata

--- src/App.js
+++ src/App.js
@@ -6,29 +6,52 @@ function App() {
       <div className='GameContainer'>
         <div className='BracketContainer'>
           <div className='RO16ContainerB'>
-            <div className='RO16ContainerS'>
-              <Item name="فلان الفلاني" />
-              <Item name="فلان الفلاني" />
+            <div className='Ro16Bracket'>
+              <div className='RO16ContainerS'>
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
+              <img src={require('./assets/brackets/ro16-L-N.png')} alt='' />
             </div>
-            <div className='RO16ContainerS'>
-              <Item name="فلان الفلاني" />
-              <Item name="فلان الفلاني" />
+            <div className='Ro16Bracket'>
+              <div className='RO16ContainerS'>
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
+              <img src={require('./assets/brackets/ro16-L-N.png')} alt='' />
             </div>
-            <div className='RO16ContainerS'>
-              <Item name="فلان الفلاني" />
-              <Item name="فلان الفلاني" />
+            <div className='Ro16Bracket'>
+              <div className='RO16ContainerS'>
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
+              <img src={require('./assets/brackets/ro16-L-N.png')} alt='' />
             </div>
-            <div className='RO16ContainerS'>
-              <Item name="فلان الفلاني" />
-              <Item name="فلان الفلاني" />
+            <div className='Ro16Bracket'>
+              <div className='RO16ContainerS'>
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
+              <img src={require('./assets/brackets/ro16-L-N.png')} alt='' />
             </div>
 
           </div>
           <div className='RO8ContainerB'>
-            <Item name="فلان الفلاني" />
-            <Item name="فلان الفلاني" />
-            <Item name="فلان الفلاني" />
-            <Item name="فلان الفلاني" />
+            <div className='RO8Bracket'>
+              <div className='RO8ContainerS'>            
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
+              <img src={require('./assets/brackets/Ro8-L-N.png')} alt='' />
+            </div>
+            <div className='RO8Bracket'>
+              <div className='RO8ContainerS'>            
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
+              <img src={require('./assets/brackets/Ro8-L-N.png')} alt='' />
+            </div>
+
           </div>
           <div className='RO4ContainerB'>
             <Item name="فلان الفلاني" />
@@ -41,29 +64,52 @@ function App() {
             <Item name="فلان الفلاني" />
           </div>
           <div className='RO8ContainerB'>
-            <Item name="فلان الفلاني" />
-            <Item name="فلان الفلاني" />
-            <Item name="فلان الفلاني" />
-            <Item name="فلان الفلاني" />
+            <div className='RO8Bracket'>
+            <img src={require('./assets/brackets/Ro8-R-N.png')} alt='' />
+              <div className='RO8ContainerS'>            
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
+            </div>
+            <div className='RO8Bracket'>
+            <img src={require('./assets/brackets/Ro8-R-N.png')} alt='' />
+
+              <div className='RO8ContainerS'>            
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
+            </div>
+
           </div>
           <div className='RO16ContainerB'>
-            <div className='RO16ContainerS'>
-              <Item name="فلان الفلاني" />
-              <Item name="فلان الفلاني" />
+            <div className='Ro16Bracket'>
+              <img src={require('./assets/brackets/ro16-R-N.png')} alt=''/>
+              <div className='RO16ContainerS'>
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
             </div>
-            <div className='RO16ContainerS'>
-              <Item name="فلان الفلاني" />
-              <Item name="فلان الفلاني" />
+            <div className='Ro16Bracket'>
+              <img src={require('./assets/brackets/ro16-R-N.png')} alt='' />
+              <div className='RO16ContainerS'>
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
             </div>
-            <div className='RO16ContainerS'>
-              <Item name="فلان الفلاني" />
-              <Item name="فلان الفلاني" />
+            <div className='Ro16Bracket'>
+              <img src={require('./assets/brackets/ro16-R-N.png')} alt=''/>
+              <div className='RO16ContainerS'>
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
             </div>
-            <div className='RO16ContainerS'>
-              <Item name="فلان الفلاني" />
-              <Item name="فلان الفلاني" />
+            <div className='Ro16Bracket'>
+              <img src={require('./assets/brackets/ro16-R-N.png')} alt=''/>
+              <div className='RO16ContainerS'>
+                <Item name="فلان الفلاني" />
+                <Item name="فلان الفلاني" />
+              </div>
             </div>
-
           </div>
 
         </div>
